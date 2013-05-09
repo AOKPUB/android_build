2e5b925da0d9107fbef01f1f08c427c326a346b1
shane new <shanenew13@gmail.com>
2013-05-09 02:41:01 -0500

    Update product_config.mk

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,7 +185,7 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-  ifneq ($(AOKP_PRODUCT),)
+  ifneq ($(AOKPUB_PRODUCT),)
     all_product_configs := $(shell ls vendor/aokpub/products/${AOKPUB_PRODUCT}.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
